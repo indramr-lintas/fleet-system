20aad21ba22d4eb4737ffe101076972f8035431a
indramr-lintas <indramr.lintas@gmail.com>
2026-03-24 16:49:22 +0700

    loader.py

--- modules/loader.py
+++ modules/loader.py
@@ -1,34 +1,23 @@
+import streamlit as st
 import gspread
 import pandas as pd
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 
-#FILE = "data/fleet_management_template.xlsx"
 
 def load_data():
 
-    scope = [
-        "https://spreadsheets.google.com/feeds",
-        "https://www.googleapis.com/auth/drive"
-    ]
+    creds_dict = st.secrets["gcp_service_account"]
 
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        "fleet-system-491104-112c658e4fb2.json", scope
-    )
+    creds = Credentials.from_service_account_info(creds_dict)
 
     client = gspread.authorize(creds)
 
     sheet = client.open("fleet_database")
 
-    
     master = pd.DataFrame(sheet.worksheet("MASTER_KENDARAAN").get_all_records())
     dokumen = pd.DataFrame(sheet.worksheet("DOKUMEN_KENDARAAN").get_all_records())
     km = pd.DataFrame(sheet.worksheet("MONITORING_KM").get_all_records())
     qc = pd.DataFrame(sheet.worksheet("QC_INSPECTION").get_all_records())
-
-    #master = pd.read_excel(FILE, sheet_name="MASTER_KENDARAAN")
-    #dokumen = pd.read_excel(FILE, sheet_name="DOKUMEN_KENDARAAN")
-    #km = pd.read_excel(FILE, sheet_name="MONITORING_KM")
-    #qc = pd.read_excel(FILE, sheet_name="QC_INSPECTION")
 
     # Bersihkan data KM
     km["KM_UPDATE"] = pd.to_numeric(
@@ -42,5 +31,3 @@ def load_data():
     )
 
     return master, dokumen, km, qc
-
-
