f1ab528669e969287877b635d7708df066b3e29a
indramr <indramr.lintas@gmail.com>
2026-04-13 10:45:21 +0700

    update feature qc monitoring

--- modules/maintenance_prediction.py
+++ modules/maintenance_prediction.py
@@ -5,11 +5,7 @@ def maintenance_prediction(km):
     km["KM_UPDATE"] = pd.to_numeric(km["KM_UPDATE"], errors="coerce")
     km["KM_SERVICE_NEXT"] = pd.to_numeric(km["KM_SERVICE_NEXT"], errors="coerce")
 
-    prediction = km[
-        (km["KM_SERVICE_NEXT"] - km["KM_UPDATE"] <= 1000) &
-        (km["KM_SERVICE_NEXT"] - km["KM_UPDATE"] > 0)
-    ]
+    # 🔥 inti logic
+    km["SISA_KM_SERVICE"] = km["KM_SERVICE_NEXT"] - km["KM_UPDATE"]
 
-    prediction["SISA_KM_SERVICE"] = km["KM_SERVICE_NEXT"] - km["KM_UPDATE"]
-
-    return prediction.sort_values("SISA_KM_SERVICE")
+    return km
